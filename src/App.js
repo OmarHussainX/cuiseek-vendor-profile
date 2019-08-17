02f75df5595c0fe913f30c51f259f03490197d6c
Omar Hussain <omar.hussain@gmail.com>
2019-08-17 11:59:43 -0600

    added placeholder onClick for 'Edit profile' button

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react'
 import { Button, Card, DatePicker, Tabs, message } from 'antd'
 import 'antd/dist/antd.css'
 
-const { TabPane } = Tabs
-const operations = <Button>Edit Profile</Button>
 
 class App extends React.Component {
   state = {
     date: null,
   }
-
+  
+  
   handleChange = date => {
     message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`)
     this.setState({ date })
   }
 
+  editProfile = () => {
+    message.info('Add modal to edit account');
+  }
+
   render() {
     const { date } = this.state
+    const { TabPane } = Tabs
+    const operations = <Button type="primary" onClick={this.editProfile}>Edit Profile</Button>
+
     return (
       <div style={{ width: '80%', margin: '100px auto' }}>
         <DatePicker onChange={this.handleChange} />
@@ -26,22 +32,20 @@ class App extends React.Component {
         <Tabs tabBarExtraContent={operations}>
           <TabPane tab="Membership" key="1">
             Content of Membership tab
-    </TabPane>
+          </TabPane>
           <TabPane tab="Security" key="2">
             Content of Security tab
-    </TabPane>
+          </TabPane>
           <TabPane tab="Bookings" key="3">
             Content of Bookings tab
-    </TabPane>
+          </TabPane>
         </Tabs>
         <div style={{ background: '#ECECEC', padding: '30px' }}>
           <Card title="Card one" bordered={false} style={{ width: 300 }}>
             <p>Card content</p>
             <p>Card content</p>
-            <p>Card content</p>
           </Card>
           <Card title="Card two" bordered={false} style={{ width: 300 }}>
-            <p>Card content</p>
             <p>Card content</p>
             <p>Card content</p>
           </Card>
