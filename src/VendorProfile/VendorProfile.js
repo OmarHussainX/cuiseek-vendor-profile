069d179a35bc5a99727672e87ef02fd7cc94763f
Omar Hussain <omar.hussain@gmail.com>
2019-08-17 15:26:57 -0600

    maore layout progress, starting to layout area for map

--- src/VendorProfile/VendorProfile.js
+++ src/VendorProfile/VendorProfile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BackTop, Button, Card, Tabs, message, Row, Col, Avatar, Typography, Descriptions } from 'antd'
 import 'antd/dist/antd.css'
 import './VendorProfile.css'
-import { company, bookings } from './data'  //using faker to generate vendor data!
+import { company, bookings, contact, address } from './data'  //using faker to generate vendor data!
 
 
 class VendorProfile extends React.Component {
@@ -23,20 +23,22 @@ class VendorProfile extends React.Component {
   render() {
     const { TabPane } = Tabs
     const { Title } = Typography
+    const ownerStatus = contact.isOwner ? <em>Business owner</em> : 'Name'
 
     console.log('company from data.js/faker', company)
 
     return (
       <div>
         <BackTop />
-        <Row gutter={16} style={{ marginBottom: 20 }}>
+        {/* --------------------------------------------------------------------- */}
+        <Row gutter={16}>
           <Col span={6}>
             <Avatar size={128} src={company.profileImageUrl} />
           </Col>
           <Col span={12}>
             <Title level={4}>{company.name}</Title>
             <Descriptions layout="vertical" size="small" colon={false}>
-            <Descriptions.Item label={bookings.count}>No. of bookings</Descriptions.Item>
+              <Descriptions.Item label={bookings.count}>No. of bookings</Descriptions.Item>
               <Descriptions.Item label={bookings.count + ' min.'}>Time saved</Descriptions.Item>
             </Descriptions>
           </Col>
@@ -50,23 +52,36 @@ class VendorProfile extends React.Component {
             </Button>
           </Col>
         </Row>
-        <Tabs>
+        {/* --------------------------------------------------------------------- */}
+        <Tabs style={{ marginTop: 32 }}>
           <TabPane tab="Overview" key="1">
-            {/* <div style={{ background: '#ECECEC', padding: '30px' }}> */}
             <div>
-              <Card bordered={true} style={{ }}>
-              <Title level={4}>General information</Title>
-            {/* {company.phone}<br /> */}
-            {/* <a href={company.websiteUrl}>{company.websiteUrl}</a> */}
-            <Descriptions layout="vertical" size="small" colon={false}>
-              <Descriptions.Item label={company.email.toLowerCase()}>E-mail</Descriptions.Item>
-              <Descriptions.Item label={company.websiteUrl}>Website</Descriptions.Item>
-              <Descriptions.Item label={company.phone}>Phone</Descriptions.Item>
-            </Descriptions>
+              <Card bordered={true} style={{}}>
+                <Title level={4}>General information</Title>
+                <Descriptions layout="vertical" size="small" colon={false}>
+                  <Descriptions.Item label={company.email.toLowerCase()}>E-mail</Descriptions.Item>
+                  <Descriptions.Item label={company.websiteUrl}>Website</Descriptions.Item>
+                  <Descriptions.Item label={company.phone}>Phone</Descriptions.Item>
+                </Descriptions>
               </Card>
-              <Card title="Card two" bordered={true} style={{ width: 300, marginTop: 20 }}>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card bordered={true} style={{ marginTop: 20}}>
+                <Title level={4}>Contact information</Title>
+                <Descriptions layout="vertical" size="small" colon={false}>
+                  <Descriptions.Item label={`${contact.firstName} ${contact.lastName}`}>{ownerStatus}</Descriptions.Item>
+                  <Descriptions.Item label={contact.jobTitle}>Title</Descriptions.Item>
+                  <Descriptions.Item label={contact.email}>E-mail</Descriptions.Item>
+                  <Descriptions.Item label={contact.phone}>Phone</Descriptions.Item>
+                </Descriptions>
+              </Card>
+              <Card bordered={true} style={{ marginTop: 20}}>
+                <Title level={4}>Address</Title>
+                <Descriptions layout="vertical" size="small" colon={false}>
+                  <Descriptions.Item label={`${address.streetSecondaryAddress}, ${address.streetAddress}`}>Address</Descriptions.Item>
+                  <Descriptions.Item label={address.city}>City</Descriptions.Item>
+                  <Descriptions.Item label={address.state}>State</Descriptions.Item>
+                  <Descriptions.Item label={address.zipCode}>Postal code</Descriptions.Item>
+                  <Descriptions.Item label={address.country}>Country</Descriptions.Item>
+                </Descriptions>
               </Card>
             </div>
           </TabPane>
@@ -86,7 +101,8 @@ class VendorProfile extends React.Component {
             Content of Membership tab
           </TabPane>
         </Tabs>
-        <div>
+        {/* --------------------------------------------------------------------- */}
+        {/* <div>
           <Row gutter={16}>
             <Col span={8}>col-8</Col>
             <Col span={8}>col-8</Col>
@@ -98,7 +114,7 @@ class VendorProfile extends React.Component {
             <Col span={6}>col-6</Col>
             <Col span={6}>col-6</Col>
           </Row>
-        </div>
+        </div> */}
       </div>
     )
   }
